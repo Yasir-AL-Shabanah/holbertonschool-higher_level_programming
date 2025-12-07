1c4109565ed0952869785c023173237d320990b7
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2025-12-07 15:49:13 +0000

    fix pep8 E501; enforce 3-line map; pass checks

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """Return a new matrix with squares of input values (no in-place change)."""
-    if matrix is None:
-        return None
     return [[n * n for n in row] for row in matrix]
