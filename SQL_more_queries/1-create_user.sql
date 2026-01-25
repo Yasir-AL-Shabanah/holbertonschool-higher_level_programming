56a6017d3a9a85f4f57809d998965b4011b2049b
Yasir-AL-Shabanah <Yaser.al.shabanah@gmail.com>
2026-01-25 13:25:16 +0000

    Fix SQL_more_queries tasks 1 and 2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,3 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Creates MySQL user 'user_0d_1' with full privileges and grant option
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
